fbe9a66a7c25c2ef5db1245517b718937c0863c3
AbbyPy <34009194+AbbyPy@users.noreply.github.com>
2023-11-11 14:12:03 +0100

    Create homework1.c & comando

---
+++ homework1.c
@@ -0,0 +1,14 @@
+/*
+
+Scrivere un programma che riceva in input una sequenza di interi positivi terminata immettendo un numero gi`a dato
+in input; questo ultimo dato non deve essere considerato ma solo fungere da terminatore della sequenza.
+Se a0, . . . , an `e la sequenza inserita, sia b0, . . . , bn la sequenza ordinata degli input, in modo tale che la funzione
+definita da π(bi) = ai (0 ≤ i ≤ n) sia una permutazione di {b0, . . . , bn}.
+Il programma deve scrivere la permutazione π in notazione ciclica. L’output del programma deve essere la sequenza
+de cicli di π scritti fra parentesi tonde, ordinati lessicograficamente, omettendo i cicli di lunghezza 1; ad esempio,
+all’input
+2 3 29 8 12 32 30 3
+deve corrispondere l’output
+(8 29 12)(30 32)
+
+  */
